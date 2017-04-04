ba7398d6a5fa372b81e56e3c9e9d8dd54826c112
jrojo <jrojomo@gmail.com>
2017-04-04 09:49:47 +0200

    feature sample in two steps

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -24,7 +24,7 @@ class FeatureContext implements Context
         $this->driver = new \Behat\Mink\Driver\GoutteDriver();
         $this->session = new \Behat\Mink\Session($this->driver);
         $this->session->start();
-        $this->base_url = "http://192.168.1.38/clubdev";
+        $this->base_url = "http://192.168.1.41/clubdev";
     }
 
     /**
@@ -33,10 +33,6 @@ class FeatureContext implements Context
     public function iAccessApiV0Samplem()
     {
         $this->session->visit($this->base_url . "/api/v1.0/sample");
-        $page = $this->session->getPage();
-        $text = $page->getContent();
-        if (strcmp($text, "{\"Usuarios\":[{\"nombre\":\"V\u00edctor\",\"Apellido\":\"Robles\"},{\"nombre\":\"Antonio\",\"Apellido\":\"Martinez\"}]}"))
-            throw new Exception('The text differs from expected');
 //        throw new \Behat\Behat\Tester\Exception\PendingException();
     }
 
@@ -45,6 +41,10 @@ class FeatureContext implements Context
      */
     public function iReceiveProperDataInJsonFormat()
     {
-        throw new \Behat\Behat\Tester\Exception\PendingException();
+        $page = $this->session->getPage();
+        $text = $page->getContent();
+        if (strcmp($text, "{\"Usuarios\":[{\"nombre\":\"V\u00edctor\",\"Apellido\":\"Robles\"},{\"nombre\":\"Antonio\",\"Apellido\":\"Martinez\"}]}"))
+            throw new Exception('The text differs from expected');
+//        throw new \Behat\Behat\Tester\Exception\PendingException();
     }
 }
